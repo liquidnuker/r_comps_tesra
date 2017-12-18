cb6db195240cd0b5440dc0fb204ea49f8c54b710
administrator <liquidnuker@users.noreply.github.com>
2017-12-18 09:46:55 -0800

    modified purifycss webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,10 @@ module.exports = {
         path.join(__dirname, '*.html'),
         path.join(__dirname, 'src/vue-components/*.vue')
       ]),
+      purifyOptions: {
+        whitelist: [ '*:not*' ]
+      },
+      minimize: true
     })
 
   ],
